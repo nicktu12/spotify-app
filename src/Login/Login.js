eae9930362c1df55a18e203b1d482e0a8439f891
Nick Teets <nickteets@gmail.com>
2017-10-24 19:38:53 -0600

    Update package so maybe this thing will get off the ground

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -13,7 +13,20 @@ class Login extends React.Component {
   render(){
     return(
       <div>
-        This is login div
+        <div class="container">
+      <div id="login">
+	<h1>Heyo this is nick's super crazy spotify application test login yo</h1>
+        <a href="/login" class="btn btn-primary">Log in with Spotify</a>
+      </div>
+      <div id="loggedin">
+        <div id="user-profile">
+        </div>
+        <div id="oauth">
+        </div>
+        <button class="btn btn-default" id="obtain-new-token">Obtain new token using the refresh token</button>
+      </div>
+    </div>
+
       </div>
     )
   }
