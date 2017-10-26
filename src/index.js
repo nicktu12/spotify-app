2199bd3b44b0fb00903bd72ca11b676aaa8e8a72
Nick Teets <nickteets@gmail.com>
2017-10-26 11:40:36 -0600

    Implement sass and preprocessor

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store, { history } from './Utilities/store';
 import registerServiceWorker from './Utilities/registerServiceWorker';
-import './index.css';
 import App from './App/App';
+import './index.css';
 
 const target = document.getElementById('root');
 
