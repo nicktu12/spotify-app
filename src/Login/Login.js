54fbb6e07310611be216731b6ef773d70cdff6d0
Nick Teets <nickteets@gmail.com>
2017-10-25 19:08:45 -0600

    Begin firebase install, update gitignore for swap files

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,32 +1,57 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import '../firebase.js';
 
 class Login extends React.Component {
   constructor() {
     super();
 
     this.state = {
-	
+      username: '',
+      song: ''      
     }
+  }
+
+  handleInput = (e) => {
+    this.setState({
+	[e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
   }
 
   render(){
     return(
       <div>
-        <div class="container">
-      <div id="login">
-	<h1>Heyo this is nick's super crazy spotify application test login yo</h1>
-        <a href="/login" class="btn btn-primary">Log in with Spotify</a>
-      </div>
-      <div id="loggedin">
-        <div id="user-profile">
+        <div className="container">
+	  <div id="login">
+	    <h1>Heyo this is nick's super crazy spotify application test login yo</h1>
+	    <a href="/login" className="btn btn-primary">Log in with Spotify THIS WILL REFRESH PAGE</a> 
+	  </div>
+	  <div id="loggedin">
+	    <div id="user-profile">
+	    </div>
+	    <div id="oauth">
+	    </div>
+	    <button className="btn btn-default" id="obtain-new-token">Obtain new token using the refresh token THIS DOES NOTHING</button>
+	  </div>
+	</div>
+	<div>
+	  <form onSubmit={this.handleSubmit}>
+	    <p>FORM</p>
+	    <input type="text" name="username" placeholder="What yo name?" onChange={this.handleInput} value={this.state.username} />
+	    <input type="text" name="song" placeholder="song?" onChange={this.handleInput} value={this.state.song} />
+	    <button>IMABUTTON</button>
+	  </form>
         </div>
-        <div id="oauth">
-        </div>
-        <button class="btn btn-default" id="obtain-new-token">Obtain new token using the refresh token</button>
-      </div>
-    </div>
-
+        <section className="display-item">
+          <div className="wrapper">
+	    <ul>
+	    </ul>
+          </div>
+        </section>
       </div>
     )
   }
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyA_F2OgeMgzWHrl8FQcyGDG5Jqd4OYCwME",
+  authDomain: "spotify-app-246e7.firebaseapp.com",
+  databaseURL: "https://spotify-app-246e7.firebaseio.com",
+  projectId: "spotify-app-246e7",
+  storageBucket: "spotify-app-246e7.appspot.com",
+  messagingSenderId: "38109752261"
+};
+
+firebase.initializeApp(config);
