20952a7d497d17787a4b1d8e92df3715b58c3d07
Nick Teets <nickteets@gmail.com>
2017-10-29 20:11:25 -0600

    Playlists are showing up on the page

--- src/Playlist/Playlist.js
+++ src/Playlist/Playlist.js
@@ -10,23 +10,27 @@ class Playlist extends React.Component{
 		this.props.loadSongs(this.props.accessToken);	
 	}
 	
-  renderSongs = (array) => {
+  renderSongs = (array) => (
   	array.map((song, index) => (
-  		<li></li>
+  		<li key={'top songs ' + index}>{song.title} - {song.artists}</li>
   	))
-  }
+  )
 
 	render(){
 		return(		
 	    <div className='playlist-div'>
     		<h2>Your Top 40</h2>
+				<ul>
+					{this.props.topSongs && this.renderSongs(this.props.topSongs)}
+				</ul>
       </div>
   	)
 	}
 }
 
 const mapStateToProps = store => ({
-	accessToken: store.accessToken
+	accessToken: store.accessToken,
+	topSongs: store.topSongs
 })
 
 const mapDispatchToProps = dispatch => {
