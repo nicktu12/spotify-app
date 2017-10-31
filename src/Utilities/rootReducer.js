b1e7f6b509cee62eede1f21ef14677744156cf49
Nick Teets <nickteets@gmail.com>
2017-10-31 11:11:50 -0600

    Make some changes during checkin, refactor and added some comments

--- src/Utilities/rootReducer.js
+++ src/Utilities/rootReducer.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
-import { routerReducer} from 'react-router-redux';
 import { accessToken, topArtistsAction } from '../Login/Login-reducer';
 import { authCode } from '../Home/Home-reducer';
 import { topSongs } from '../Playlist/Playlist-reducer';
 
 export default combineReducers({
-	routing: routerReducer,
 	authCode, 
 	accessToken, 
 	topArtistsAction,
