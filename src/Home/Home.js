899352da8dfcb979d1b9ae7b015a080f75a2a256
Nick Teets <nickteets@gmail.com>
2017-10-31 09:54:37 -0600

    Get redirect to login page implemented

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router';
 import { authCodeCleaner } from '../Utilities/helpers';
 import { saveAuthCodeAction } from './Home-actions'; 
 
@@ -16,15 +17,22 @@ class Home extends React.Component{
 
 
 	renderTopArtists = (array) => (
-					array.map((artist, index) => (
-									<li key={'top artists ' + index}><span>{artist.name}</span></li>
-					))			
+		array.map((artist, index) => (
+			<li key={'top artists ' + index}><span>{artist.name}</span></li>
+		))			
 	) 
+
+	renderRedirect = () => {
+		if (!this.props.token.length) {
+			return (<div> <Redirect to='/login' /> </div>)
+		}			
+	} 
 
 	render(){
 		return(
 		
  			<div className='home-div'>
+				{this.renderRedirect()}
 			  <h2>Top Artists</h2>
 				<ol>
 					{this.props.topArtists && this.renderTopArtists(this.props.topArtists)}
@@ -36,7 +44,8 @@ class Home extends React.Component{
 }
 
 const mapStateToProps = store => ({
-	topArtists: store.topArtistsAction.topArtists
+	topArtists: store.topArtistsAction.topArtists,
+	token: store.accessToken
 })
 
 const mapDispatchToProps = dispatch => {
