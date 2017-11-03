562733c9b14636e264b7b0c12f2507f70da458d8
Nick Teets <nickteets@gmail.com>
2017-11-03 08:40:57 -0600

    Fixes spelling error in home actions test

--- src/Home/Home-actions.test.js
+++ src/Home/Home-actions.test.js
@@ -2,7 +2,7 @@ import * as actions from './Home-actions';
 import { mockAuthCode } from '../Utilities/mockData';
 
 describe('Home actions', ()=> {
-	it('should create an action to detech authorization code', ()=>{
+	it('should create an action to detect authorization code', ()=>{
 		const expectedAction = {
 			type: 'AUTH_CODE',
 			code: mockAuthCode 
